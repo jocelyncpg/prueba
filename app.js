6077b28ceb86f506dc1da8b5fa459b8ba6df13ec
jocelyncpg <jocperezg@gmail.com>
2024-05-16 01:49:00 -0400

    3

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const parrafo = document.getElementById("warnings")
 const apellido = document.getElementById("apellido")
 const ciudad = document.getElementById("ciudad")
 const telefono = document.getElementById("telefono")
+const escritura = document.getElementById("escritura")
 
 form.addEventListener("submit", e=>{
     e.preventDefault()
@@ -34,32 +35,4 @@ form.addEventListener("submit", e=>{
     }
 })
 
-
-form.addEventListener("submit", e=>{
-    e.preventDefault()
-    let warnings = ""
-    let entrar = false
-    parrafo.innerHTML = ""
-    if(nombre.value.length <6){
-        warnings += `El nombre no es valido <br>`
-        entrar = true
-    }
-    if(apellido.value.length <8){
-        warnings += `El apellido no es valido <br>`
-        entrar = true
-    }
-    if(ciudad.value.length <6){
-        warnings += `La ciudad no es valida <br>`
-        entrar = true
-    }
-    if(telefono.value.length <8){
-        warnings += `El telefono no es valido <br>`
-        entrar = true
-    }
-    if(entrar){
-        parrafo.innerHTML = warnings
-    }else{
-        parrafo.innerHTML = "Envio exitoso"
-    }
-})
 
