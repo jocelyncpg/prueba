34aeb9573c06c973feb5320fe43a8fbfe173478c
jocelyncpg <jocperezg@gmail.com>
2024-05-16 01:37:32 -0400

    validaciones js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const email = document.getElementById("email")
 const pass = document.getElementById("password")
 const form = document.getElementById("form")
 const parrafo = document.getElementById("warnings")
+const apellido = document.getElementById("apellido")
+const ciudad = document.getElementById("ciudad")
+const telefono = document.getElementById("telefono")
 
 form.addEventListener("submit", e=>{
     e.preventDefault()
@@ -30,3 +33,33 @@ form.addEventListener("submit", e=>{
         parrafo.innerHTML = "Enviado"
     }
 })
+
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    parrafo.innerHTML = ""
+    if(nombre.value.length <6){
+        warnings += `El nombre no es valido <br>`
+        entrar = true
+    }
+    if(apellido.value.length <8){
+        warnings += `El apellido no es valido <br>`
+        entrar = true
+    }
+    if(ciudad.value.length <6){
+        warnings += `La ciudad no es valida <br>`
+        entrar = true
+    }
+    if(telefono.value.length <8){
+        warnings += `El telefono no es valido <br>`
+        entrar = true
+    }
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "Envio exitoso"
+    }
+})
+
